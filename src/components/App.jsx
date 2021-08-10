b5d3ea7895176fc13e75c166e475da674538e3fe
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2021-08-10 16:14:47 -0400

    completed naviation functionality for going to next and previous books

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,28 @@ class App extends Component {
         };
     }
 
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPrevious = () =>{
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
     render(){
         return (
             <div className="container-fluid"> 
@@ -22,6 +44,7 @@ class App extends Component {
                 <div className="row" > 
                     <div className="col-md-4"> 
                         {/* Button here to move to the previous book */}
+                        <button onClick={this.goToPrevious}>Previous book</button>
                     </div>
                     <div className="col-md-4"> 
                         {/* Display book with cover here */}
@@ -30,6 +53,7 @@ class App extends Component {
                     </div>
                     <div className="col-md-4"> 
                         {/* Button here to move to the previous book */}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div>
                 </div>
             </div>

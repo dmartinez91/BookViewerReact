a956f0c35d6bba2c894fb9953a70b391cabdff81
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2021-08-11 13:07:06 -0400

    refactored footer files

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Footer.css'
 
-function Footer(){
+const Footer = () => {
     return ( 
-        <div className="row row-rowspace">
-        <div className="col-md-12" style={{padding: 10}}>
-            <div  className="Footer" >
-            <p> dCC Library C 2021 </p>
-            </div>
-        </div>
-    </div>
+        <footer className='Footer'>
+            <h4>Book Viewer 2021</h4>
+        </footer>
      );
 }
  
 export default Footer;
+
+// function Footer(){
+//     return ( 
+//         <div className="row row-rowspace">
+//         <div className="col-md-12" style={{padding: 10}}>
+//             <div  className="Footer" >
+//             <p> dCC Library C 2021 </p>
+//             </div>
+//         </div>
+//     </div>
+//      );
+// }
